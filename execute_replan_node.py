5f853a19aa9b7cf32a1a78efba817f0dd19012d4
严宇强 <805709525@qq.com>
2025-11-07 18:29:56 +0800

    修改bug

--- execute_replan_node.py
+++ execute_replan_node.py
@@ -121,13 +121,16 @@ async def execute_replan_node(state: PlanExecute) -> PlanExecute:
         )
         if project_status != "分析失败！" and project_status != "执行失败！":
             break
-        if len(project_status) > config["SUMMARY_MAX_LENGTH"]:
-            project_status = summary_pro.invoke(
-                f"请适当总结项目实际状况，输出结果控制在{config['SUMMARY_MAX_LENGTH']}个token以内，项目实际状况内容如下：\n{project_status}"
-            ).content.strip()
-            with open(
-                f"./dist/{config['PROJECT_NAME']}/development_log.md", "w", encoding="utf-8"
-            ) as f:
+        
+        development_info = ""
+        with open(
+            f"./dist/{config['PROJECT_NAME']}/development_log.md", "w+", encoding="utf-8"
+        ) as f:
+            development_info = f.read()
+            if len(development_info) > config["SUMMARY_MAX_LENGTH"]:
+                development_info = summary_pro.invoke(
+                    f"请适当总结项目实际状况，输出结果控制在{config['SUMMARY_MAX_LENGTH']}个token以内，项目实际状况内容如下：\n{development_info}"
+                ).content.strip()
                 f.write(project_status)
 
         analysis_count += 1
